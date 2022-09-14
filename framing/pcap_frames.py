2d4a0317bd9655c0f4348babc070f8606256151d
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2022-09-14 08:09:51 +0300

    PCAP frame header

---
+++ framing/pcap_frames.py
@@ -0,0 +1,20 @@
+from framing.base import *
+from framing.frames import *
+
+# https://datatracker.ietf.org/doc/id/draft-gharris-opsawg-pcap-00.html
+
+
+class PCAP(BaseFrame['PCAP']):
+    fields = Structure['PCAP']()
+
+    Magic_Number = fields.raw(bytes=4)
+    Major_Version = fields.integer(bytes=2)
+    Minor_Version = fields.integer(bytes=2)
+    Reserved1 = fields.raw(bytes=4)
+    Reserved2 = fields.raw(bytes=4)
+    SnapLen = fields.integer(bytes=4)
+    LinkType = fields.integer(bytes=4)
+
+
+
+
