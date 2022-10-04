bf49a07db5a209fae529546e1f4bc1e48288546d
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2022-10-04 13:30:04 +0300

    Count IP payload types

--- framing/pcap_analyzer.py
+++ framing/pcap_analyzer.py
@@ -5,6 +5,7 @@ import pathlib
 from typing import Dict, List
 
 from framing.frame_types.ethernet_frames import Ethernet_Payloads, EthernetII
+from framing.frame_types.ipv4_frames import IPv4
 from framing.frames import Frames
 from framing.frame_types.pcap_frames import PCAPFile, PCAP_Payloads, PacketRecord, FileHeader
 from framing.raw_data import Raw
@@ -17,6 +18,7 @@ class PCAPScanner:
         self.file_count = 0
         self.pcap_frame_count = 0
         self.ethernet_data_type_count: Dict[int, int] = {}
+        self.ip_data_type_count: Dict[int, int] = {}
 
     def scan_files(self, file_list: List[pathlib.Path]):
         for file in file_list:
@@ -32,7 +34,6 @@ class PCAPScanner:
 
     def scan_pcap_file(self, file: pathlib.Path):
         raw_data = Raw.file(file)
-        offset = 0
         try:
             pcap = PCAPFile(Frames.dissect(raw_data))
             PCAP_Payloads.add_to(pcap)
@@ -40,14 +41,18 @@ class PCAPScanner:
 
             hdr = PCAPFile.File_Header[pcap]
             assert FileHeader.LinkType[hdr] == 1, "Non-Ethernet capture not supported"
-            offset += hdr.get_bit_length()
 
+            frame_num = 0
             for i, rec in enumerate(PCAPFile.Packet_Records.iterate(pcap)):
-                offset += rec.get_bit_length()
+                frame_num += 1
                 self.pcap_frame_count += 1
                 eth = PacketRecord.Packet_Data[rec]
-                data_type = EthernetII.type[eth]
-                self.ethernet_data_type_count[data_type] = self.ethernet_data_type_count.get(data_type, 0) + 1
+                eth_dt = EthernetII.type[eth]
+                self.ethernet_data_type_count[eth_dt] = self.ethernet_data_type_count.get(eth_dt, 0) + 1
+                if eth_dt == 0x0800:
+                    ip = EthernetII.data[eth]
+                    ip_td = IPv4.Protocol[ip]
+                    self.ip_data_type_count[ip_td] = self.ip_data_type_count.get(ip_td, 0) + 1
         finally:
             raw_data.close()
 
@@ -59,7 +64,11 @@ class PCAPScanner:
 
         r.append("Ethernet payload types:")
         for t, c in self.ethernet_data_type_count.items():
-            r.append(f"  {t:04x}: {c}")
+            r.append(f"  0x{t:04x}: {c}")
+
+        r.append("IP payload types:")
+        for t, c in self.ip_data_type_count.items():
+            r.append(f"  0x{t:02x}: {c}")
         return "\n".join(r)
 
 
