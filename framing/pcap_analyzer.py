0c891bba19cf3ade5e32cdf2f8d8461fc5212f57
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2022-10-05 09:01:04 +0300

    PCAP scan by directories

--- framing/pcap_analyzer.py
+++ framing/pcap_analyzer.py
@@ -13,17 +13,20 @@ from framing.raw_data import Raw
 
 class PCAPScanner:
     """Scan PCAPs for attack surface measurements"""
-    def __init__(self):
+    def __init__(self, name=""):
         self.logger = logging.getLogger("scanner")
+        self.name = name
         self.file_count = 0
         self.pcap_frame_count = 0
         self.ethernet_data_type_count: Dict[int, int] = {}
         self.ip_data_type_count: Dict[int, int] = {}
 
-    def scan_files(self, file_list: List[pathlib.Path]):
+    def scan_files(self, file_list: List[pathlib.Path], limit=0):
         for file in file_list:
+            if limit and self.file_count >= limit:
+                return
             if file.is_dir():
-                self.scan_files(list(file.iterdir()))
+                self.scan_files(list(file.iterdir()), limit)
                 continue
             if not file.suffix == ".pcap":
                 self.logger.info("skip file %s", file.as_posix())
@@ -58,30 +61,56 @@ class PCAPScanner:
             raw_data.close()
 
     def __repr__(self):
-        r = [
+        r = []
+        if self.name:
+            r.append(f"## {self.name} ##")
+        r.extend([
             f"PCAP files:   {self.file_count}",
             f"PCAP frames:  {self.pcap_frame_count}",
-        ]
+        ])
 
         r.append("Ethernet payload types:")
-        for t, c in self.ethernet_data_type_count.items():
+        for t, c in sorted(self.ethernet_data_type_count.items()):
             r.append(f"  0x{t:04x}: {c}")
 
         r.append("IP payload types:")
-        for t, c in self.ip_data_type_count.items():
+        for t, c in sorted(self.ip_data_type_count.items()):
             r.append(f"  0x{t:02x}: {c}")
         return "\n".join(r)
+
+
+def separate_scans_by_directory(roots: List[pathlib.Path], limit=0) -> List[PCAPScanner]:
+    r = []
+    for rf in roots:
+        if not rf.is_dir():
+            continue
+        for f in rf.iterdir():
+            if limit and len(r) >= limit:
+                return r
+            scn = PCAPScanner(f.name)
+            scn.scan_files(list(f.iterdir()))
+            r.append(scn)
+    return r
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("files", action="append", help="PCAPs file or directories to read")
+    parser.add_argument("--by-dir", action="store_true", help="Different scan for each subdirectory")
+    parser.add_argument("--limit", type=int, default=0, help="Limit number of scans or files (ease of testing)")
     parser.add_argument("-l", "--log", dest="log_level", choices=['DEBUG', 'INFO', 'WARNING', 'ERROR', 'CRITICAL'],
                         help="Set the logging level", default=None)
     args = parser.parse_args()
     logging.basicConfig(format='%(message)s', level=getattr(logging, args.log_level or 'INFO'))
+    files = [pathlib.Path(n) for n in args.files]
+    limit = args.limit
 
-    scanner = PCAPScanner()
-    scanner.scan_files([pathlib.Path(n) for n in args.files])
-    print(f"{scanner}")
+    if args.by_dir:
+        scans = separate_scans_by_directory(files, limit)
+        for s in scans:
+            print(f"{s}")
+    else:
+        scanner = PCAPScanner()
+        scanner.scan_files(files, limit)
+        print(f"{scanner}")
 
