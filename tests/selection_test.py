6a7acb578ba5ae00e334d8a8e714963d27292ed5
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2022-10-15 17:10:58 +0300

    Selection tests were not in vc

---
+++ tests/selection_test.py
@@ -0,0 +1,41 @@
+from framing.base import Frame
+from framing.fields import Selection, Structure
+from framing.frames import Frames
+from framing.raw_data import Raw
+
+
+class ASelection(Frame):
+    structure = Selection['ASelection']()
+
+    A = structure.choice(1, structure.integer(bytes=2))
+    B = structure.choice(2, structure.raw(bytes=8))
+    C = structure.choice(3, structure.integer(bytes=4))
+
+
+class XFrame(Frame):
+    structure = Structure['XFrame']()
+
+    type = structure.integer(bytes=1)
+    value = structure.sub(ASelection).choice_by(type)
+
+
+def test_selection():
+    x = XFrame(Frames.compose())
+
+    b = XFrame.value.select(x, ASelection.B)
+    assert b.byte_length() == 8
+    assert b.encode() == Raw.hex("0000 0000 0000 0000")
+
+    ASelection.C[b] = 0x1234
+    assert b.encode() == Raw.hex("0000 1234")
+
+    assert x.encode() == Raw.hex("03 0000 1234")
+
+
+def test_selection_decode():
+    x = XFrame(Frames.dissect(Raw.hex("03 0123 4567 89")))
+    assert XFrame.type[x] == 3
+    v = XFrame.value[x]
+    assert v.encode() == Raw.hex("01234567")
+
+
