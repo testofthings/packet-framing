b9ce29372644b3e2d1b2f2d634089b4224025181
Rauli Kaksonen <rauli.kaksonen@icloud.com>
2022-11-29 13:44:52 +0200

    Setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='framing',
+    version='0.0',
+    author="Rauli Kaksonen",
+    author_email="rauli.kaksonen@gmail.com",
+    description='Frame parsing for good people',
+    long_description='Under construction',
+    packages=['framing'],
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
