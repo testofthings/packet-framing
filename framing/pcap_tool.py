dd7b645d3cab7bbe5741d600106020dc1d22874b
Rauli Kaksonen <43605561+raulikak@users.noreply.github.com>
2024-03-27 21:12:06 +0200

    Renaming pcap tool code by the idea of 'stack'

--- framing/pcap_tool.py
+++ framing/pcap_tool.py
@@ -14,51 +14,56 @@ from framing.frames import Frames
 from framing.raw_data import Raw, RawData
 
 
-class FrameStack:
+class StackState:
     """Extractor output, frame and way to get payload data"""
-    def __init__(self, lower: List[Frame], data: RawData):
-        self.lower = lower
+    def __init__(self, data: RawData, lower: List[Frame] = None):
         self.data = data
+        self.lower = [] if lower is None else lower
 
+    def add(self, data: RawData, frame: Frame):
+        """Add frame to the stack"""
+        lower = self.lower.copy()
+        lower.append(frame)
+        return StackState(data, lower)
 
-class FrameExtractor:
-    """Extract frames from raw data using a specification"""
+class FrameStackLayer:
+    """Frame stack layer"""
     def __init__(self):
-        self.next: Dict[Any, FrameExtractor] = {}
+        self.next: Dict[Any, FrameStackLayer] = {}
 
-    def extract(self, input: FrameStack) -> Iterable[Frame]:
-        """Extract frames from raw data"""
-        return [RawFrame(Frames.dissect(input.data))]
+    def input(self, state: StackState) -> Iterable[Frame]:
+        """Input through the stack"""
+        return [RawFrame(Frames.dissect(state.data))]
 
     def build(self, spec: Dict[Any, Any]):
         """Build this extractor recursively"""
         for k, v in spec.items():
             next = None
             if k == 'pcap':
-                next = self.next[k] = PCAPRecordExtractor()
+                next = self.next[k] = PCAPStackLayer()
             if k == 'eth':
-                next = self.next[1] = PayloadFieldExtractor(EthernetII, EthernetII.type, EthernetII.data)
+                next = self.next[1] = PayloadFieldStackLayer(EthernetII, EthernetII.type, EthernetII.data)
             # FIXME: Reassembly for payloads
             if k == 'ip4':
-                next = self.next[0x0800] = PayloadFieldExtractor(IPv4, IPv4.Protocol, IPv4.Payload)
+                next = self.next[0x0800] = PayloadFieldStackLayer(IPv4, IPv4.Protocol, IPv4.Payload)
             if k == 'ip6':
-                next = self.next[0x86dd] = PayloadFieldExtractor(IPv6, IPv6.Next_header, IPv6.Payload)
+                next = self.next[0x86dd] = PayloadFieldStackLayer(IPv6, IPv6.Next_header, IPv6.Payload)
             if next and v and isinstance(v, Dict):
                 next.build(v)
 
 
-class RootExtractor(FrameExtractor):
-    def extract(self, input: FrameStack) -> Iterable[Frame]:
+class FrameStack(FrameStackLayer):
+    def input(self, state: StackState) -> Iterable[Frame]:
         if not self.next:
-            return super().extract(input)
+            return super().input(state)
         next = self.next.values().__iter__().__next__()
-        return next.extract(input)
+        return next.input(state)
 
 
-class PCAPRecordExtractor(FrameExtractor):
+class PCAPStackLayer(FrameStackLayer):
     """Extract PCAP records"""
-    def extract(self, input: FrameStack) -> Iterable[Frame]:
-        file = PCAPFile(Frames.dissect(input.data))
+    def input(self, state: StackState) -> Iterable[Frame]:
+        file = PCAPFile(Frames.dissect(state.data))
         hdr = PCAPFile.File_Header[file]
         if not self.next:
             yield hdr
@@ -70,16 +75,10 @@ class PCAPRecordExtractor(FrameExtractor):
                 yield rec
             else:
                 pay_data = PacketRecord.Packet_Data[rec]
-                output = FrameStack(input.lower + [rec], pay_data)
-                out_d = next.extract(output)
-                for out in out_d:
-                    yield out
+                output = state.add(pay_data, rec)
+                yield from next.input(output)
 
-
-class TypedFieldExtractor(FrameExtractor):
-    pass
-
-class PayloadFieldExtractor(TypedFieldExtractor):
+class PayloadFieldStackLayer(FrameStackLayer):
     """Extract frame from payload field"""
     def __init__(self, frame_type: Type[Frame], type_field: Field, payload_field: RawField):
         super().__init__()
@@ -89,8 +88,8 @@ class PayloadFieldExtractor(TypedFieldExtractor):
         self.type_field = type_field
         self.payload_field = payload_field
 
-    def extract(self, input: FrameStack) -> Iterable[Frame]:
-        frame = self.frame_type(Frames.dissect(input.data))
+    def input(self, state: StackState) -> Iterable[Frame]:
+        frame = self.frame_type(Frames.dissect(state.data))
         if not self.next:
              yield frame
         type_v = self.type_field[frame]
@@ -98,8 +97,8 @@ class PayloadFieldExtractor(TypedFieldExtractor):
         if next is None:
             return []
         pay_raw = self.payload_field[frame]
-        output = FrameStack(input.lower + [frame], pay_raw)
-        out_d = next.extract(output)
+        output = state.add(pay_raw, frame)
+        out_d = next.input(output)
         yield from out_d
 
     def __repr__(self):
@@ -118,15 +117,15 @@ def main():
     # add space after colon to faciliate more compact command-line notation
     filter_s = filter_s.replace(':{', ': {')
     filter_d = yaml.safe_load(filter_s)
-    extractor = RootExtractor()
-    extractor.build(filter_d or {})
+    stack = FrameStack()
+    stack.build(filter_d or {})
 
     # print extracted frames from files
     for file in args.read or []:
         f = pathlib.Path(file)
         data = Raw.file(f)
         try:
-            for fr in extractor.extract(FrameStack([], data)):
+            for fr in stack.input(StackState(data)):
                 print(f"{fr}")
         finally:
             data.close()
