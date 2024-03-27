063cfb6db274bf4f54ebc6612ced1999c73c149f
Rauli Kaksonen <43605561+raulikak@users.noreply.github.com>
2024-03-27 14:21:40 +0200

    PCAP tool pcap and eth extraction draft

--- framing/pcap_tool.py
+++ framing/pcap_tool.py
@@ -1,45 +1,70 @@
 import argparse
 import pathlib
-from typing import Any, Callable, Dict, Iterable, Type
+from typing import Any, Callable, Dict, Iterable, List, Optional, Type
 import yaml
 
 from framing.backends import RawFrame
 from framing.base import Field, Frame
+from framing.frame_types.ethernet_frames import EthernetII
 from framing.frame_types.pcap_frames import PCAPFile, PCAPRecordIterator, PacketRecord
 from framing.frames import Frames
 from framing.raw_data import Raw, RawData
 
 
+class FrameOutput:
+    """Extractor output, frame and way to get payload data"""
+    def __init__(self, frame: Frame, payload: Field = None):
+        self.frame = frame
+        self.payload = payload
+
+
 class FrameExtractor:
     """Extract frames from raw data using a specification"""
     def __init__(self, spec: Dict[Any, Any]):
-        self._build(spec)
         self.process: Callable[[RawData], Iterable[Frame]] = self._no_processing
+        self.next: Optional[FrameExtractor] = None
         self._build(spec)
-
-    def _no_processing(self, data: RawData) -> Frame:
-        return [RawFrame(Frames.dissect(data))]
 
     def _build(self, spec: Dict[Any, Any]):
         """Build this extractor recursively"""
         for k, v in spec.items():
             if k == 'pcap':
                 self.process = self._extract_pcap_records
+            if k == 'eth':
+                self.process = self._extract_ethernet_frames
+            if isinstance(v, Dict) and v:
+                self.next = FrameExtractor(v)
 
-    def _extract_pcap_records(self, data: RawData) -> Iterable[RawData]:
+    def _no_processing(self, data: RawData) -> Iterable[FrameOutput]:
+        return [FrameOutput(RawFrame(Frames.dissect(data)))]
+
+    def _extract_pcap_records(self, data: RawData) -> Iterable[FrameOutput]:
         """Extract PCAP records"""
         file = PCAPFile(Frames.dissect(data))
         hdr = PCAPFile.File_Header[file]
-        yield hdr
+        yield FrameOutput(hdr)
         for i, rec in enumerate(PCAPRecordIterator(file)):
-            yield rec
+            yield FrameOutput(rec, PacketRecord.Packet_Data)
 
-    def extract(self, data: RawData) -> Iterable[Frame]:
+    def _extract_ethernet_frames(self, data: RawData) -> Iterable[FrameOutput]:
+        """Extract Ethernet frames"""
+        frame = EthernetII(Frames.dissect(data))
+        return [FrameOutput(frame, EthernetII.data)]
+
+    def extract(self, data: RawData, stack: List[Frame] = None) -> Iterable[FrameOutput]:
+        if stack is None:
+            stack = []
         while data:
             frame_it = self.process(data)
-            for frame in frame_it:
-                yield frame
-                frame_len = frame.byte_length()
+            for out in frame_it:
+                if self.next:
+                    if out.payload:
+                        sub_data = out.payload[out.frame]
+                        for sub in self.next.extract(sub_data, stack + [out.frame]):
+                            yield sub
+                else:
+                    yield out
+                frame_len = out.frame.byte_length()
                 data = data.tailBytes(frame_len)
 
 
@@ -59,7 +84,7 @@ def main():
         with pathlib.Path(file) as f:
             data = Raw.file(f)
             for fr in extractor.extract(data):
-                print(f"{fr}")
+                print(f"{fr.frame}")
 
 
 if __name__ == '__main__':
