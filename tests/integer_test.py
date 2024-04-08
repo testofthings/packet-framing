4bfc70f09036941e6b9e35a3d6c4b2c2874fc008
Rauli Kaksonen <43605561+raulikak@users.noreply.github.com>
2024-04-08 19:57:13 +0300

    Direct bit integer fix

--- tests/integer_test.py
+++ tests/integer_test.py
@@ -23,3 +23,25 @@ def test_fixed_bit_int():
     assert codec.encode(14) == Raw.bits("1110")
 
     assert codec.decode(b1)
+
+
+def test_direct_one_bit_int():
+    codec = IntegerFormat(bits=1).create_codec()
+    assert codec.decode_direct(0, Raw.hex("7f")) == 0
+    assert codec.decode_direct(0, Raw.hex("80")) == 1
+    assert codec.decode_direct(1, Raw.hex("bf")) == 0
+    assert codec.decode_direct(1, Raw.hex("40")) == 1
+    assert codec.decode_direct(2, Raw.hex("df")) == 0
+    assert codec.decode_direct(2, Raw.hex("20")) == 1
+    assert codec.decode_direct(7, Raw.hex("fe")) == 0
+    assert codec.decode_direct(7, Raw.hex("01")) == 1
+
+
+def test_direct_int():
+    raw = Raw.hex("11 00 3e 38 96")
+    codec = IntegerFormat(bits=8).create_codec()
+    assert codec.decode_direct(0, raw) == 0x11
+    assert codec.decode_direct(4, raw) == 0x10
+    assert codec.decode_direct(8, raw) == 0x00
+    assert codec.decode_direct(12, raw) == 0x03
+    assert codec.decode_direct(16, raw) == 0x3e
